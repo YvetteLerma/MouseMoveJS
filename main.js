ab0304f49dd481a5802f30bf2b35a81cc2c10f78
Yvette Lerma <lerma.yvette@hotmail.com>
2024-01-26 13:23:24 -0700

    finished this project

--- main.js
+++ main.js
@@ -15,7 +15,12 @@ function shadow(e) {
     const xWalk = (x / width * walk) - (walk / 2);
     const yWalk = (y / width * walk) - (walk / 2); //this gives you pixel values
 
-    text.style.textShadow = `${xWalk}px ${yWalk}px 0 red`;
+    text.style.textShadow = `
+        ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+        ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7)
+        ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
+        ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
+        `; //you can see the values change in the console when hovering over the text - in the h1
 }
 
 hero.addEventListener('mousemove', shadow);
